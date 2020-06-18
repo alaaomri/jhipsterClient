a8244dac7efa558de97166ae7f7eeefbd6971787
alaaomri <omrialaaeddine@gmail.com>
2020-06-18 23:47:52 +0100

    [add french as second langage]

--- src/main/webapp/app/config/translation.ts
+++ src/main/webapp/app/config/translation.ts
@@ -1,4 +1,4 @@
-import { TranslatorContext, Storage } from 'react-jhipster';
+import { Storage, TranslatorContext } from 'react-jhipster';
 
 import { setLocale } from 'app/shared/reducers/locale';
 
@@ -7,6 +7,7 @@ TranslatorContext.setRenderInnerTextForMissingKeys(false);
 
 export const languages: any = {
   en: { name: 'English' },
+  fr: { name: 'French' },
   // jhipster-needle-i18n-language-key-pipe - JHipster will add/remove languages in this object
 };
 
